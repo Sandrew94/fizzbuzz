f47ff2c9cbd260a5137ede8f64a96e310de726a6
Sandrew94 <64556313+Sandrew94@users.noreply.github.com>
2021-06-05 07:44:04 +0200

    [COMPLETE] Step 3 - Add rules for multiple of 7,11 and both together

--- fizzBuzz-Game.js
+++ fizzBuzz-Game.js
@@ -40,11 +40,30 @@ const fizzBuzz2 = function (min, max) {
 
 //Step 3
 
+const rangeMinMax1 = function (min, max) {
+  return Array.from(Array(max - min + 1), (_, idx) => min + idx);
+};
+
+const fizzBuzz3 = function (min, max) {
+  const numberTo = rangeMinMax1(min, max); //Add a range
+  numberTo.map((val, idx, arr) => {
+    if (val % 3 === 0 && val % 5 === 0) return (arr[idx] = "FizzBuzz"); //multiple of 3 and 5 and replace their value with "FizzBuzz"
+    if (val % 3 === 0) return (arr[idx] = "Fizz"); //multiple of 3 and replace his value with "Fizz"
+    if (val % 5 === 0) return (arr[idx] = "Buzz"); //multiple of 5 and replace his value with "Buzz"
+    if (val % 7 === 0 && val % 11 === 0) return (arr[idx] = "FooBoo");
+    if (val % 7 === 0) return (arr[idx] = "Foo");
+    if (val % 11 === 0) return (arr[idx] = "Boo");
+  });
+  return numberTo;
+};
+
+//console.log(fizzBuzz3(1, 77));
+
 //ALL STEPS
 const functions = {
   step1: fizzBuzz1,
   step2: fizzBuzz2,
+  step3: fizzBuzz3,
 };
-//console.log(functions.step2);
 
 module.exports = functions;
